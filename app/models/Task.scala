87bed3cfdf3bbb3f2a830cd3e6b357aa7a101c9b
VadSm <vadsmirnov2@gmail.com>
2020-02-21 11:18:25 +0300

    Remove useless object

--- app/models/Task.scala
+++ app/models/Task.scala
@@ -5,12 +5,6 @@ import play.api.libs.json.Json
 case class Task(id: Long, title: String, completed: Boolean, priority: String)
 
 object Task {
-  // We're going to be serving this type as JSON, so specify a
-  // default Json formatter for our Task type here
-  implicit val format = Json.format[Task]
-}
-
-object Task {
   implicit val reads: Reads[Task] = for {
       id <- (__ \ "id").readWithDefault[Long](0L)
       title <- (__ \ "title").read[String]
